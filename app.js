fd2ea9595e98fb0b0325f43d376464ff7ba19b11
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-17 11:15:31 -0500

    Creando lista funcional

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigosAgregados = [];
+function agregarAmigo(){
+    //localizamos el nombre y captamos el valor
+    let nombre = document.getElementById('amigo').value;
+    //almacenamos en la lista
+    listaAmigosAgregados.push(nombre);
+    //verificamos en la consola
+    console.log(listaAmigosAgregados);
+} 
